8e4aa291eccf4f52296926e66626ecdaa6aed132
jaleks3 <s24592@pjwstk.edu.pl>
2024-02-12 15:09:43 +0100

    Created first test endpoint

--- StudentsAPI/src/main/java/com/example/StudentsAPI/StudentsApiApplication.java
+++ StudentsAPI/src/main/java/com/example/StudentsAPI/StudentsApiApplication.java
@@ -1,13 +1,20 @@
 package com.example.StudentsAPI;
 
+import com.example.StudentsAPI.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class StudentsApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentsApiApplication.class, args);
 	}
-
 }
