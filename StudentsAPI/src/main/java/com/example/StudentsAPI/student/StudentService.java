23740ffcd324b8e60a8b016fd71b1a40c8fe9e6b
jaleks3 <s24592@pjwstk.edu.pl>
2024-02-12 15:15:12 +0100

    Added correct annotations

--- StudentsAPI/src/main/java/com/example/StudentsAPI/student/StudentService.java
+++ StudentsAPI/src/main/java/com/example/StudentsAPI/student/StudentService.java
@@ -1,11 +1,14 @@
 package com.example.StudentsAPI.student;
 
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
 
+@Service
 public class StudentService {
 
     public List<Student> getStudents() {
