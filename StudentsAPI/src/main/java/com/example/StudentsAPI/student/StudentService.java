6a013e907185bd39c572c11647b7b3899f64bd47
jaleks3 <s24592@pjwstk.edu.pl>
2024-02-12 17:16:32 +0100

    Implemented DELETE endpoint

--- StudentsAPI/src/main/java/com/example/StudentsAPI/student/StudentService.java
+++ StudentsAPI/src/main/java/com/example/StudentsAPI/student/StudentService.java
@@ -3,6 +3,7 @@ package com.example.StudentsAPI.student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentService {
@@ -16,5 +17,20 @@ public class StudentService {
 
     public List<Student> getStudents() {
         return studentRepository.findAll();
+    }
+
+    public void addNewStudent(Student student) {
+        Optional<Student> studentByEmail = studentRepository.findStudentByEmail(student.getEmail());
+
+        if(studentByEmail.isPresent())
+            throw new IllegalStateException("email taken");
+
+        studentRepository.save(student);
+    }
+    public void deleteStudent(Long studentId) {
+        if(!studentRepository.existsById(studentId))
+            throw new IllegalStateException("student with given id: " + studentId + " does not exist");
+
+        studentRepository.deleteById(studentId);
     }
 }
